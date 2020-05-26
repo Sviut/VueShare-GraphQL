d13cbd085ed4451af8b073ac9aea6d22111c2054
Sviut <44402566+Sviut@users.noreply.github.com>
2020-05-26 19:37:43 +0300

    add check token jwt

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer, AuthenticationError } = require('apollo-server')
 const mongoose = require('mongoose')
 
 require('dotenv').config({ path: 'dev.env' })
 const fs = require('fs')
 const path = require('path')
+const jwt = require('jsonwebtoken')
 
 const filePath = path.join(__dirname, 'typeDefs.graphql')
 const typeDefs = fs.readFileSync(filePath, 'utf-8')
@@ -16,13 +17,25 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('DB connected'))
   .catch(e => console.log(e))
 
+const getUser = async (token) => {
+  if (token) {
+    try {
+      let user = await jwt.verify(token, process.env.SECRET)
+      console.log(user)
+    } catch (e) {
+      throw new AuthenticationError('Your session has ended. Please sign in again.')
+    }
+  }
+}
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {
-    User,
-    Post
-  }
+  context: async ({ req }) => {
+    const token = req.headers['authorization'] || ''
+
+    return { User, Post, currentUser: await getUser(token) }
+  },
 })
 
 server.listen().then(({ url }) => {
